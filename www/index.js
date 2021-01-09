e848f0455a4c1e873941086229842ab8f200e917
Evgeniy Rashchepkin <e.a.rashchepkin@gmail.com>
2021-01-09 18:41:49 +0500

    Add Play/Pause

--- www/index.js
+++ www/index.js
@@ -1,22 +1,45 @@
 import { Board } from 'life';
 
 const board = Board.for_canvas('canvas', 1600, 800);
-board.fill_with_random(400, 0.3);
+board.fill_with_random(400, 0.8);
 board.render();
 
+const turnLabel = document.getElementById('turn');
+
 const next = count => {
-  console.time(`next${count}`);
+  // console.time(`next${count}`);
   for (let i = 0; i < count; i++) {
     board.next();
   }
-  console.timeEnd(`next${count}`);
+  // console.timeEnd(`next${count}`);
 
   // console.time('render');
   board.render();
   // console.timeEnd('render');
 
-  document.getElementById('turn').textContent = board.turn.toString();
+  turnLabel.textContent = board.turn.toString();
 }
+
+let animationId = null;
+
+const renderLoop = () => {
+  next(1);
+  animationId = requestAnimationFrame(renderLoop);
+};
+
+const playPauseBtn = document.getElementById("playPauseBtn");
+
+playPauseBtn.addEventListener('click', () => {
+  if (animationId === null) {
+    playPauseBtn.textContent = '⏸';
+    renderLoop();
+  }
+  else {
+    playPauseBtn.textContent = '▶';
+    cancelAnimationFrame(animationId);
+    animationId = null;
+  }
+});
 
 document.getElementById('nextBtn1').addEventListener('click', () => next(1));
 document.getElementById('nextBtn10').addEventListener('click', () => next(10));
